7aef01642fdfbdacc4f81ade7fc2d6941b15493a
hayato94087 <hayato94087@gmail.com>
2024-04-03 06:23:29 +0900

    feat:isolatedModulesを無効化した場合の挙動を確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { type FC } from "react";
+import { User } from "@/lib/re-export";
+
+const user: User = {
+  name: "John",
+  age: 30,
+};
 
 const Home: FC = () => {
+  console.log(user);
+
   return (
     <div className="">
       <div className="text-lg font-bold">Home</div>
